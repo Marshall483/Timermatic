0ae0f87c380ab3363de82c14e2b910710ba3d531
Alexander Putilin <jows.17@mail.ru>
2021-02-20 22:25:18 +0300

    6 video

---
+++ MYFIRSTPROJECT/MyConole/MyConole/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace MyConole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(config =>
+            {
+                    config.UseStartup<Startup>();
+            });
+    }
+}
---
+++ MYFIRSTPROJECT/MyConole/MyConole/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyConole
+{
+    public class Startup
+    {
+
+        public void ConfigurationSrevices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseRouting();
+
+            app.UseEndpoints(ep =>
+            {
+                ep.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Timerchan!");
+                });
+            });
+
+        }
+    }
+}
